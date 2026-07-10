6c76587c76c093277331a76bc9a36d842aa06056
S M Tanvir Tanmoy <92587839+tanvirtanmoy@users.noreply.github.com>
2026-07-10 12:56:02 +0200

    Add: sql-sales-person - submission-10

---
+++ SQL/sql-sales-person/submission-10.sql
@@ -0,0 +1,19 @@
+-- sales_person, orders , company
+WITH CTE AS
+(
+SELECT s.sales_id, s.name
+FROM sales_person AS s
+INNER JOIN orders AS o 
+ON s.sales_id = o .sales_id
+INNER JOIN company AS c
+ON o.com_id = c.com_id
+AND c.name = 'CRIMSON'
+)
+SELECT s.name
+FROM sales_person AS s
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM CTE 
+    WHERE 
+    s.sales_id = CTE.sales_id
+)
